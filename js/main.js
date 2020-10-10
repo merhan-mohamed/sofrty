1a08206a98cc999df19a8ad514a1478ca57f0a78
merhan-mohamed <merhan_mohamed89@hotmail.com>
2020-10-10 04:05:22 +0200

    final update

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
 $(document).ready(function(){
-    $('i.fa-star').click(function(){
-        $(this).toggleClass('stars')
-    })
-
-});
 $('#IM').click(function(){
     $(this).attr('type','file');
 
 });
 $( function() {
     $( "#datepicker" ).datepicker();
-  } );
+  });
   
   $( function() {
     $( "#datepicker1" ).datepicker();
-  } );
+  });
+})
